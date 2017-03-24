1ebcb1a256be483fa13323e2cae7ad1823f681c7
Demon <demonsingur@gmail.com>
2017-03-24 07:46:00 +0000

    ResourceLoader: add load, loadAll and helper methods

--- js/ResourceLoader.js
+++ js/ResourceLoader.js
@@ -9,14 +9,14 @@
         s.load = function(cb) {
             s.content.onload = function() {
                 s.loaded = true;
-                cb();
+                cb(s);
             };
             s.content.src = op.src;
         };
+
         s.get = function(op) {
             return s.content;
         };
-        
     }
 
     function ResourceLoader() {
@@ -28,13 +28,58 @@
         
         rl.setType = function(name, fn) {
             rl.types[name] = fn; 
-        } ;
+        };
+
         rl.add = function(name, type, op) {
             rl.resources[name] = new rl.types[type](op);
         };
-        rl.get = function(name, op, cb) {
-            rl.resources[name].get(op, cb);
-        }
+
+        rl.get = function(name, op) {
+            return rl.resources[name].get(op);
+        };
+
+        rl.load = function(name, cb) {
+            rl.resources[name].load(cb);
+        };
+
+        rl.loadAllSync = function(cb) {
+            var i = 0;
+            var s = function() {
+                rl.resources[i].load(function() {
+                    if(i == rl.resources.length) {
+                        cb(rl.resources);
+                    } else {
+                        i++;
+                        s();
+                    }
+                });
+            };
+            s();
+        };
+
+        rl.loadAllAsync = function(cb) {
+            rl.resources.forEach(function(r) {
+                r.load(function() {
+                    var all = true;
+                    rl.resources.forEach(function(r) {
+                        if(!r.loaded) {
+                            all = false;
+                        }
+                    });
+                    if(all) {
+                        cb(rl.resources);
+                    }
+                });
+            });
+        };
+
+        rl.loadAll = function(cb, sync) {
+            if(sync) {
+                rl.loadAllSync(cb);
+            } else {
+                rl.loadAllAsync(cb);
+            }
+        };
     }
     window.ResourceLoader = ResourceLoader;
 })();
